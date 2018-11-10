ebac4aeb475ab499208c7f801156c68c6566c15b
Chunjie Lu <lvchunjie00@gmail.com>
2018-11-09 23:22:13 -0500

    add header

--- product.py
+++ product.py
@@ -9,3 +9,8 @@ print(product)
 
 for p in product:
 	print(p[0],'的价格是', p[1])
+
+with open ('product.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,价格\n')
+	for p in product:
+		f.write(p[0] + ',' + p[1] + '\n')
