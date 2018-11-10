9b6abda26dab1f030e60007d59cbb8e27561012a
Chunjie Lu <lvchunjie00@gmail.com>
2018-11-09 23:49:17 -0500

    add read profile

--- product.py
+++ product.py
@@ -1,4 +1,13 @@
+#读取档案
 product =[]
+with open('product.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,价格' in line:
+			continue
+		name, price = line.strip().split(',')
+		product.append([name, price])
+
+#让使用者输入
 while True:
 	name = input('请输入商品名称:')
 	if name == "q":
@@ -7,9 +16,11 @@ while True:
 	product.append([name,price])
 print(product)
 
+#印出所有购买记录
 for p in product:
 	print(p[0],'的价格是', p[1])
 
+#写入档案
 with open ('product.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,价格\n')
 	for p in product:
